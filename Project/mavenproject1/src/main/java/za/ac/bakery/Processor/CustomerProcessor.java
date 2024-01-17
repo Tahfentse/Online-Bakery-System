75b15c81ced5e39315b9cf576d3dd0cb10f97426
Tahfentse <fentse283@gmail.com>
2024-01-17 09:46:24 +0200

    Adding files

---
+++ Project/mavenproject1/src/main/java/za/ac/bakery/Processor/CustomerProcessor.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.bakery.Processor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import za.ac.bakery.Controller.ProcessRequest;
+
+/**
+ *
+ * @author Train
+ */
+public class CustomerProcessor extends ProcessRequest {
+
+    @Override
+    public void Request(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+}
--- Project/mavenproject1/src/main/java/za/ac/bakery/Processor/OrderProcessor.java
+++ Project/mavenproject1/src/main/java/za/ac/bakery/Processor/OrderProcessor.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package za.ac.bakery.Controller;
+package za.ac.bakery.Processor;
 
 /**
  *
  * @author Train
  */
-public class CustomerController {
+public class OrderProcessor {
     
 }
